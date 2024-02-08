35ed61822d2b81ed06c00cd623f3a655c576b99a
Subrata Das <84288189+akashdas786@users.noreply.github.com>
2024-02-08 13:36:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use Strict";  // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs , not brower
+
+let name = "Subrata Das"
+
+let age = 45
+
+let IsLoggedIn = false 
+
+
+// number => 2 to power 32
+//  bigInt 
+// string =>""
+//boolean => true / false 
+// null => standalone value 
+// undefind  variable declaration ✅ , value assign ❌
+// symbol => unique 
+
+
+//object 
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null); // return as a object 
+console.log(typeof undefind);
