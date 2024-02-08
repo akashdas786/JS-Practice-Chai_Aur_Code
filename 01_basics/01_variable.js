d0214b5c02f4b5b3e538da412490697e46f9afcd
Subrata Das <84288189+akashdas786@users.noreply.github.com>
2024-02-08 12:19:20 +0000

    main

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,31 @@
+const accountId =1446575  
+let accountEmail="subratadas@gmail.com"
+var accountPassword="1648653"
+accountCity="Kolkata"
+let accountState;
+
+
+// accountId=2 // not allowed 
+
+accountEmail="gsacahcgacdb"
+accountPassword="654896455"
+accountCity="bengaluru"
+
+
+//      console.log(accountState);
+
+
+console.log(accountId);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountEmail);
+
+/* 
+
+prefer not  to use var
+ beacause of issue in block scope and functional scope  
+
+
+*/
+
+console.table([accountEmail, accountId , accountPassword , accountCity]) // tabular form 
