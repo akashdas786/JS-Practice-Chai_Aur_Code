7d59cdd889e57df170fe87d85f972f371f5adbf3
Subrata Das <84288189+akashdas786@users.noreply.github.com>
2024-02-08 16:09:39 +0000

    tricky Questions about dataTypes

---
+++ 01_basics/04_dt_strToNumber.js
@@ -0,0 +1,25 @@
+let value = 33
+
+let Negvalue= -value
+console.log(Negvalue);
+
+
+let str1 = "hello"
+let str2 = "subrata"
+
+let str3 = str1+ str2
+
+console.log(str3);
+
+
+console.log("1"+2)
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+let gameCounter = 100
+
+gameCounter++;
+
+console.log(gameCounter);
